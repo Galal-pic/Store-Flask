b6d15c327b4f68b3722e6483233df83ba138cabf
Galal Ewida <galalamrewida@gmail.com>
2024-12-25 21:32:47 +0200

    end the first vedio

--- backend/models.py
+++ backend/models.py
@@ -20,3 +20,16 @@ class Recipe(db.Model):
     def delete(self):
         db.session.delete(self)
         db.session.commit()
+
+#user model
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
